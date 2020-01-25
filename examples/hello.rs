b2079728d7cde122656fff8922ca5ebf99b36794
Aleksandrov Vladimir <invis87@gmail.com>
2020-01-25 15:23:28 +0300

    blink works for STM32F103C8T6 controller

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,20 +1,62 @@
 //! Prints "Hello, world!" on the host console using semihosting
 
+
 #![no_main]
 #![no_std]
 
-extern crate panic_halt;
+//extern crate panic_halt;
+//extern crate stm32f103xx;
+//extern crate stm32f1xx;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hprintln};
 
+
+//use stm32f103xx::GPIOC;
+//use stm32_hal::gpio::Port;
+
+use panic_halt as _;
+use nb::block;
+use stm32f1xx_hal;
+
+
 #[entry]
 fn main() -> ! {
     hprintln!("Hello, world!").unwrap();
+    hprintln!("Hello, second!").unwrap();
+    hprintln!("Hello, third!").unwrap();
+
+//    let cp = cortex_m::Peripherals::take().unwrap();
+//    let p = stm32f103xx::Peripherals::take().unwrap();
+//    p.PWR
+//    p.GPIOA.idr.write(|w| w.pin0);
+//
+//    let gpioc = p.GPIOA.crl.write(|x| x.enable().set_bit().mode().set_bit());
+//
+//
+//    // Get GPIOC somehow...
+//    let gpioc = GPIOC.borrow(cs);
+//    // Set pins 13, 14 and 15 on GPIOC to 1, 0 and 1.
+//    gpioc.write_pin_range(13, 3, 0b101);
 
     // exit QEMU
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
-    debug::exit(debug::EXIT_SUCCESS);
+//    debug::exit(debug::EXIT_SUCCESS);
 
-    loop {}
+    let mut x = 10;
+    loop {
+        x += 1;
+        if x % 100 == 0 {
+            hprintln!("current x = {}", x).unwrap();
+        }
+    }
+
+//    let mut x: i32  =0 ;
+//
+//    loop {
+//
+//        hprintln!("Hello from loop: {}", x).unwrap();
+//        x += 1;
+//
+//    }
 }
--- memory.x
+++ memory.x
@@ -1,11 +1,20 @@
-MEMORY
-{
+/* STM32F103x8, 64K flash, 20K RAM, bootloader will be erased */
+
+/* Define memory regions. */
+MEMORY {
+  FLASH : ORIGIN = 0x08000000, LENGTH = 64K
+  RAM : ORIGIN = 0x20000000, LENGTH = 20K
+}
+
+
+/* MEMORY
+ { */
   /* NOTE 1 K = 1 KiBi = 1024 bytes */
   /* TODO Adjust these memory regions to match your device memory layout */
   /* These values correspond to the LM3S6965, one of the few devices QEMU can emulate */
-  FLASH : ORIGIN = 0x00000000, LENGTH = 256K
+/*  FLASH : ORIGIN = 0x00000000, LENGTH = 256K
   RAM : ORIGIN = 0x20000000, LENGTH = 64K
-}
+} */
 
 /* This is where the call stack will be allocated. */
 /* The stack is of the full descending type. */
