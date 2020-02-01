1b7d00fe87df681f8f6a6d8bd5c49709533d5b75
Aleksandrov Vladimir <invis87@gmail.com>
2020-02-01 20:17:11 +0300

    use main.rs for debugging purpose

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,67 @@
+#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
-
-use cortex_m::asm;
+use stm32f1xx_hal::{
+    prelude::*,
+    pac,
+    timer::Timer,
+    delay::Delay,
+    rcc,
+};
 use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+use embedded_hal::digital::v2::OutputPin;
+use cortex_m::asm::delay;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        let location = _info.location();
+        let message = _info.message();
+        hprintln!("panic! location: {:?}, message: {:?}", location, message).unwrap();
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    hprintln!("Hello, world!").unwrap();
+    // Get access to the core peripherals from the cortex-m crate
+    let cp = cortex_m::Peripherals::take().unwrap();
+    // Get access to the device specific peripherals from the peripheral access crate
+    let dp = pac::Peripherals::take().unwrap();
 
+    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
+    // HAL structs
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
+    // `clocks`
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.mhz())
+        .sysclk(72.mhz())
+        .pclk1(36.mhz())
+        .pclk2(72.mhz())
+        .freeze(&mut flash.acr);
+
+    // Acquire the GPIOC peripheral
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+
+    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
+    // in order to configure the port. For pins 0-7, crl should be passed instead.
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+    hprintln!("clock: {:?}", clocks.sysclk().0).unwrap();
+    let second = 7200000;
     loop {
-        // your code goes here
+        delay(second * 3);
+        led.set_high().unwrap();
+
+        delay(second * 5);
+        led.set_low().unwrap();
     }
 }
