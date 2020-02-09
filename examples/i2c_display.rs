7fa20d42a5ad4fde919b757e6c819b657a4c4099
Aleksandrov Vladimir <invis87@gmail.com>
2020-02-09 23:12:49 +0300

    add text drawing to display

--- examples/i2c_display.rs
+++ examples/i2c_display.rs
@@ -14,6 +14,8 @@ use nb::block;
 use core::panic::PanicInfo;
 use cortex_m_rt::entry;
 use embedded_graphics::{image::Image, prelude::*, pixelcolor::BinaryColor};
+use embedded_graphics::fonts::{Font6x8, Text, Font6x12};
+use embedded_graphics::style::TextStyleBuilder;
 use stm32f1xx_hal::i2c::{BlockingI2c, DutyCycle, Mode};
 use stm32f1xx_hal::prelude::*;
 use stm32f1xx_hal::stm32;
@@ -83,13 +85,47 @@ fn main() -> ! {
         block!(timer.wait()).unwrap();
         disp.clear();
 
-        let shifted_image = orig_image.translate(Point::new(x_shift, 0));
-        disp.draw(shifted_image.into_iter());
+        let shift_point = Point::new(x_shift, 0);
+        let shifted_image = orig_image.translate(shift_point);
+        shifted_image.draw(&mut disp);
+        draw_text(&mut disp);
         disp.flush().unwrap();
 
         x_shift += 1;
         if x_shift >= 128 {x_shift = 0;}
     }
+}
+
+fn draw_text(disp: &mut OledDisplay) {
+    let mut buf = [0u8; 64];
+
+    let text_style_1 = TextStyleBuilder::new(Font6x12)
+        .text_color(BinaryColor::On)
+        .build();
+
+    let text_1: &str = write_to::show(
+        &mut buf,
+        format_args!("Hello world! {:?}", text_style_1.font),
+    ).unwrap();
+
+    Text::new(text_1, Point::zero())
+        .into_styled(text_style_1)
+        .draw(disp);
+
+
+    let text_style_2 = TextStyleBuilder::new(Font6x8)
+        .text_color(BinaryColor::On)
+        .build();
+
+    let text_2: &str = write_to::show(
+        &mut buf,
+        format_args!("Hello Rust! {:?}", text_style_2.font),
+    ).unwrap();
+
+    Text::new(text_2, Point::new(0, 16))
+        .into_styled(text_style_2)
+        .draw(disp);
+
 }
 
 //use cortex_m_semihosting::{debug, hprintln};
@@ -99,5 +135,61 @@ fn panic(_info: &PanicInfo) -> ! {
         let _location = _info.location();
         let _message = _info.message();
 //        hprintln!("panic! location: {:?}, message: {:?}", _location, _message).unwrap();
+    }
+}
+
+
+
+
+// from https://stackoverflow.com/questions/50200268/how-can-i-use-the-format-macro-in-a-no-std-environment
+pub mod write_to {
+    use core::cmp::min;
+    use core::fmt;
+
+    pub struct WriteTo<'a> {
+        buffer: &'a mut [u8],
+        // on write error (i.e. not enough space in buffer) this grows beyond
+        // `buffer.len()`.
+        used: usize,
+    }
+
+    impl<'a> WriteTo<'a> {
+        pub fn new(buffer: &'a mut [u8]) -> Self {
+            WriteTo { buffer, used: 0 }
+        }
+
+        pub fn as_str(self) -> Option<&'a str> {
+            if self.used <= self.buffer.len() {
+                // only successful concats of str - must be a valid str.
+                use core::str::from_utf8_unchecked;
+                Some(unsafe { from_utf8_unchecked(&self.buffer[..self.used]) })
+            } else {
+                None
+            }
+        }
+    }
+
+    impl<'a> fmt::Write for WriteTo<'a> {
+        fn write_str(&mut self, s: &str) -> fmt::Result {
+            if self.used > self.buffer.len() {
+                return Err(fmt::Error);
+            }
+            let remaining_buf = &mut self.buffer[self.used..];
+            let raw_s = s.as_bytes();
+            let write_num = min(raw_s.len(), remaining_buf.len());
+            remaining_buf[..write_num].copy_from_slice(&raw_s[..write_num]);
+            self.used += raw_s.len();
+            if write_num < raw_s.len() {
+                Err(fmt::Error)
+            } else {
+                Ok(())
+            }
+        }
+    }
+
+    pub fn show<'a>(buffer: &'a mut [u8], args: fmt::Arguments) -> Result<&'a str, fmt::Error> {
+        let mut w = WriteTo::new(buffer);
+        fmt::write(&mut w, args)?;
+        w.as_str().ok_or(fmt::Error)
     }
 }
