ac6e53f887f4383430c4e48b4a6d11e7832ea3f2
Aleksandrov Vladimir <invis87@gmail.com>
2020-02-10 00:41:34 +0300

    use Timer and Delay in blink

--- examples/blink.rs
+++ examples/blink.rs
@@ -1,14 +1,10 @@
-//! Blinks an LED
-//!
-//! This assumes that a LED is connected to pc13 as is the case on the blue pill board.
-//!
-//! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
-//! the reference manual for an explanation. This is not an issue on the blue pill.
-
 #![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+
+use nb::block;
+use cortex_m_semihosting::{debug, hprintln};
 
 use stm32f1xx_hal::{
     prelude::*,
@@ -18,7 +14,6 @@ use stm32f1xx_hal::{
     rcc,
 };
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
 use embedded_hal::digital::v2::OutputPin;
 use cortex_m::asm::delay;
 use core::panic::PanicInfo;
@@ -28,13 +23,12 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {
         let location = _info.location();
         let message = _info.message();
-        hprintln!("panic! location: {:?}, message: {:?}", location, message).unwrap();
+//        hprintln!("panic! location: {:?}, message: {:?}", location, message).unwrap();
     }
 }
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
     // Get access to the core peripherals from the cortex-m crate
     let cp = cortex_m::Peripherals::take().unwrap();
     // Get access to the device specific peripherals from the peripheral access crate
@@ -42,8 +36,8 @@ fn main() -> ! {
 
     // Take ownership over the raw flash and rcc devices and convert them into the corresponding
     // HAL structs
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    let mut flash: stm32f1xx_hal::flash::Parts = dp.FLASH.constrain();
+    let mut rcc: stm32f1xx_hal::rcc::Rcc = dp.RCC.constrain();
 
     // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
     // `clocks`
@@ -56,18 +50,21 @@ fn main() -> ! {
         .freeze(&mut flash.acr);
 
     // Acquire the GPIOC peripheral
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let mut gpioc: stm32f1xx_hal::gpio::gpioc::Parts = dp.GPIOC.split(&mut rcc.apb2);
 
     // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
     // in order to configure the port. For pins 0-7, crl should be passed instead.
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
-    hprintln!("clock: {:?}", clocks.pclk2().0).unwrap();
+    let mut timer = Timer::syst(cp.SYST, &clocks).start_count_down(5.hz()); //will fail if less than 5 (timer.rs:268)
+//    hprintln!("clock: {:?}", clocks.pclk2().0).unwrap();
     loop {
-        delay(7200000 * 3);
+//        hprintln!("first block");
+        block!(timer.wait()).unwrap();
         led.set_high().unwrap();
 
-        delay(7200000 * 5);
+//        hprintln!("second block");
+        delay(72_000_0 * 5); //have no idea why that number, but it works. Also it depends on Timer countdown
         led.set_low().unwrap();
     }
 }
