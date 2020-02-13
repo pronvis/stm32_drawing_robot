7ce0987e840c68c5a69f8738845b92a727f3798d
Aleksandrov Vladimir <invis87@gmail.com>
2020-02-14 00:20:21 +0300

    update hello.rs

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,11 +1,20 @@
 #![no_main]
 #![no_std]
-
-extern crate panic_halt;
+#![feature(panic_info_message)]
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hprintln};
+use core::panic::PanicInfo;
+use stm32f1xx_hal;
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        let location = _info.location();
+        let message = _info.message();
+        hprintln!("panic! location: {:?}, message: {:?}", location, message).unwrap();
+    }
+}
 
 #[entry]
 fn main() -> ! {
